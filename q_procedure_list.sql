61b2a7d5703e6d67d3eb764c72d85a6722334391
asep.gumilar_dxg <asep.gumilar_dxg@IU-011110>
2019-05-14 13:44:11 +0700

    initial versioning

---
+++ q_procedure_list.sql
@@ -0,0 +1,11 @@
+FA_ASSET_DESC_PUB
+
+select owner, object_name, object_type, 
+TO_CHAR(created, 'DD-MON-YYYY HH24:MI:SS') created,
+TO_CHAR(last_ddl_time, 'DD-MON-YYYY HH24:MI:SS') updated,
+status
+from dba_objects
+where owner IN ('APPS', 'XDXG')
+and object_type = 'PROCEDURE'
+and object_name = 'FA_ASSET%'--and object_type IN ('TABLE', 'VIEW', 'PACKAGE', 'PACKAGE BODY', 'PROCEDURE', 'FUNCTION', 'TRIGGER')
+order by 1, 2
